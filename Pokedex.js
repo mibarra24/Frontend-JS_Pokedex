f7154c24cb2796336d60da1a6082df73b8228abd
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-03-17 22:23:51 -0700

    commit to main

---
+++ Pokedex.js
@@ -0,0 +1,50 @@
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./Assets/pokemon-sad.gif")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+            console.log(pokeImg);
+        }
+    });
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
+ 
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#ff675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561d25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F'
+}
+
+
+
+
